24a7f9fda76e4a8be78e21566178890bfb105199
예령 <yaeryung93@gmail.com>
2026-07-10 20:52:54 +0900

    test

---
+++ src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,19 @@
+package com.example.backend.service;
+
+import com.example.backend.entity.User;
+import com.example.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User register(User user) {
+        return userRepository.save(user);
+    }
+}
